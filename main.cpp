b58d11f2386d1eb7fa5273f35ac0a1eff64c818e
16fb <55683390+16fb@users.noreply.github.com>
2020-04-05 22:38:03 +0800

    Just wanna see some testing added

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
     cout << "hello word, your dad lesbian" << endl;
-    cout << "and now we are in branc1" <<endl;
+    cout << "and now we are in branc1" << endl;
+    cout << "Just wanna see some testsing " << endl;
     return 1;
 }
